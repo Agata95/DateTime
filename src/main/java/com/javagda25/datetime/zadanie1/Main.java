3839854879c6f240f77ffd33663ae2a6748f7afe
Agata <agata.weltrowska@outlook.com>
2019-06-06 18:51:25 +0200

    poprawione

--- src/main/java/com/javagda25/datetime/zadanie1/Main.java
+++ src/main/java/com/javagda25/datetime/zadanie1/Main.java
@@ -12,32 +12,51 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
         Scanner scanner = new Scanner(System.in);
-
         String komenda;
-
         System.out.println("Dopóki nie wpiszesz QUIT wypiszę data i czas.");
 
         do {
+            System.out.println("Wybierz dostępną opcję: " + Arrays.toString(Wybor.values()));
             komenda = scanner.nextLine();
-            LocalDate data = LocalDate.now();
-            System.out.println(data);
 
-            LocalTime czas = LocalTime.now();
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-            String wynik1 = dateTimeFormatter.format(czas);
-            System.out.println(wynik1);
+            if (komenda.equalsIgnoreCase("quit")) {
+                break;
+            }
 
-            LocalDateTime dataICzas = LocalDateTime.now();
-            DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-            String wynik = dateTimeFormatter1.format(dataICzas);
-            System.out.println(wynik);
-        } while (!komenda.equalsIgnoreCase("quit"));
+            try {
+                Wybor komendy = Wybor.valueOf(komenda.toUpperCase());
+                switch (komendy) {
+                    case DATE:
+                        LocalDate data = LocalDate.now();
+                        System.out.println(data);
+                        break;
+                    case TIME:
+                        LocalTime czas = LocalTime.now();
+                        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+                        String wynik1 = dateTimeFormatter.format(czas);
+                        System.out.println(wynik1);
+                        break;
+                    case DATE_TIME:
+                        LocalDateTime dataICzas = LocalDateTime.now();
+                        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+                        String wynik = dateTimeFormatter1.format(dataICzas);
+                        System.out.println(wynik);
+                        break;
+                }
+            }catch (IllegalArgumentException iae){
+                System.err.println("Zły wybór");
+                continue;
+            }
+
+
+            } while (!komenda.equalsIgnoreCase("quit")) ;
+
     }
 }
---
+++ src/main/java/com/javagda25/datetime/zadanie1/Wybor.java
@@ -0,0 +1,7 @@
+package com.javagda25.datetime.zadanie1;
+
+public enum Wybor {
+    DATE,
+    TIME,
+    DATE_TIME
+}
